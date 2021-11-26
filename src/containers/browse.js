ab6012fb43098d3d37abfa80896d212e5a90e8cf
Dmitry Zavyalov <zavyalovdmitry@mail.ru>
2021-11-26 14:56:07 -0500

    feat: add profile pic loading spinner

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-nested-ternary */
 import React, { useContext, useState, useEffect } from 'react';
 import { SelectProfileContainer } from './profiles';
 import { FirebaseContext } from '../context/firebase';
+import { Loading } from '../components';
 
 export function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({});
@@ -15,5 +17,11 @@ export function BrowseContainer({ slides }) {
     }, 3000);
   }, [profile.displayName]);
 
-  return <SelectProfileContainer user={user} setProfile={setProfile} />;
+  return profile.displayName ? (
+    loading ? (
+      <Loading src={user.photoURL} />
+    ) : null
+  ) : (
+    <SelectProfileContainer user={user} setProfile={setProfile} />
+  );
 }
