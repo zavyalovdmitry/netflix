020f1dc3b6b9fcffc84750fdf621bc1b842cccea
Dmitry Zavyalov <zavyalovdmitry@mail.ru>
2021-11-26 14:27:42 -0500

    feat: add profiles page

---
+++ src/containers/browse.js
@@ -0,0 +1,19 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { SelectProfileContainer } from './profiles';
+import { FirebaseContext } from '../context/firebase';
+
+export function BrowseContainer({ slides }) {
+  const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
+  const { firebase } = useContext(FirebaseContext);
+  const user = firebase.auth().currentUser || {};
+
+  useEffect(() => {
+    // console.log('prof', profile);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [profile.displayName]);
+
+  return <SelectProfileContainer user={user} setProfile={setProfile} />;
+}
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { BrowseContainer } from '../containers/browse';
 import { useContent } from '../hooks';
+import selectionFilter from '../utils/selection-filter';
 
 export default function Browse() {
   const { series } = useContent('series');
-  console.log(series);
+  const { films } = useContent('films');
+  const slides = selectionFilter({ series, films });
 
-  return <p>This is browse page!</p>;
+  return <BrowseContainer slides={slides} />;
 }
