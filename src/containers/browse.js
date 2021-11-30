a9ec5d8e4e2b5126f0828b6dfba8b8d40823ef84
Dmitry Zavyalov <zavyalovdmitry@mail.ru>
2021-11-30 14:02:02 -0500

    feat: add live search

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-nested-ternary */
 import React, { useContext, useState, useEffect } from 'react';
+import Fuse from 'fuse.js';
 import { SelectProfileContainer } from './profiles';
 import { FooterContainer } from './footer';
 import { FirebaseContext } from '../context/firebase';
@@ -26,6 +27,19 @@ export function BrowseContainer({ slides }) {
   useEffect(() => {
     setSlideRows(slides[category]);
   }, [slides, category]);
+
+  useEffect(() => {
+    const fuse = new Fuse(slideRows, {
+      keys: ['data.description', 'data.title', 'data.genre'],
+    });
+    const results = fuse.search(searchTerm).map(({ item }) => item);
+
+    if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+      setSlideRows(results);
+    } else {
+      setSlideRows(slides[category]);
+    }
+  }, [searchTerm]);
 
   return profile.displayName ? (
     <>
